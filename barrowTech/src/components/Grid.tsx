5c54503468533a8fea72537cb535ab8d26013b1b
Vicki <=>
2024-10-05 15:43:46 -0700

    add grid

---
+++ barrowTech/src/components/Grid.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Grid.css';
+
+interface GridProps {
+  rows: number;
+  columns: number;
+}
+
+const Grid: React.FC<GridProps> = () => {
+  return (
+    <body>
+
+    </body>
+  );
+};
+
+export default Grid;
---
+++ barrowTech/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import Grid from "../components/Grid"
+
+function Home() {
+
+  return (
+    <>
+    <div>
+        <Grid rows={10} columns = {10}/>
+    </div>
+      
+    </>
+  )
+}
+
+export default Home
